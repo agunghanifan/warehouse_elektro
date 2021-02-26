b447be71622fea3b1bb1c25a235f517d69043fe5
TimotiusAnrez <anrez.t@gmail.com>
2021-02-26 08:42:01 +0700

    fix

---
+++ migrations/20210226012945-addBrandToProduk.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up:  (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return queryInterface.addColumn('Produks', 'BrandId',{
+      type:Sequelize.INTEGER})
+  },
+
+  down:  (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.destoryColumn('Produks','brandId',{})
+  }
+};
